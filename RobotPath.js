2680017f611e04698d9db4e61d333ff6c5cd61ba
Christopher Decker <christopher.w.decker@gmail.com>
2016-03-14 17:08:12 -0700

    [fix] add the solution and notes to the problem

--- RobotPath.js
+++ RobotPath.js
@@ -13,4 +13,60 @@ robotPaths(4);  // 184
 -- Note --
 Its recomended not to go over 6, as this will take a while...
 
+> I really like this problem because it reminds me of what you might do for an AI in a game of some sorts.  But it can be slightly challenging, always double check your math.
+
 ----------------------------------------------------------------------------- */
+
+function robotPaths (n) {
+  var makeBoard = function () {
+      var board = [];
+      for (var i = 0; i < n; i++) {
+          var row = [];
+          for (var j = 0; j < n; j++) {
+            row.push(0);
+          }
+          board.push(row);
+      }
+      return board;
+  };
+
+  var solutionCount = 0;
+  var paths = function (board, row, column) {   // ---- function start
+
+    board[row][column] = 1;  // "set" peice;
+    // console.log(board[0]);
+    // console.log(board[1], "at : ", row, column);
+    // console.log(board[2]);
+    // console.log('----------------------------');
+    if (row === n-1 && column === n-1) {
+
+      // NOTE : although you could check for your goal before placing down a "marker", when you do find your goal these console logs will print you a small map to see the path.  It helps to keep track of where you are on the "board". currently set for an argument of a 3x3 board
+
+        // console.log('ATARI');  // you win...
+        // console.log('----------------------------');
+        solutionCount++;
+        board[row][column] = 0;
+        return;
+    }
+
+    if ( row+1 < n  && board[row+1][column] === 0) {    // going "down"
+        paths(board, row+1, column);
+    }
+    if (column+1 < n && board[row][column+1] === 0) {   // going "right"
+        paths(board, row, column+1);
+    }
+    if (row-1 >= 0 && board[row-1][column] === 0) {     // going "up"
+        paths(board, row-1, column);
+    }
+    if (column-1 >= 0 && board[row][column-1] === 0) {  // going "left"
+        paths(board, row, column-1);
+    }
+
+    board[row][column] = 0;  // "remove" peice for the next itteration
+
+  };                                            // ---- function End
+
+  paths( makeBoard(), 0, 0);
+  return solutionCount;
+
+}
