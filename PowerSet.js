c8544dadcb04d19d3f1303faf57c1d8e006caf7b
Christopher Decker <christopher.w.decker@gmail.com>
2016-03-15 09:45:04 -0700

    [new] add a new problem for the day PowerSet

---
+++ PowerSet.js
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------------------
+-- Power Set --
+This function should take in a string, and then should return you all the possible combinations in alphabetical order with no repeats including an empty string.
+
+ -- NOTE --
+ A combination using the same letters even in an diffrent order will be considered a repeat.  'ab' === 'ba'
+
+ -- Example --
+powerSet('a');   --> ['', 'a']
+powerSet('ba');  --> ['', 'a', 'ab']
+powerSet('html'); --> ["", "h", "hl", "hlm", "hlmt", "hlt", "hm", "hmt", "ht", "l", "lm", "lmt", "lt", "m", "mt", "t"]
+--------------------------------------------------------------------------- */
