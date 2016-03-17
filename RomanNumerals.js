a6ae619706abb3b2342877c4303d37a6ca6f2193
Christopher Decker <christopher.w.decker@gmail.com>
2016-03-17 12:15:52 -0700

    [new] add a new problem for the day RomanNumerals

---
+++ RomanNumerals.js
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------------
+-- Roman Numerals --
+Given a string this function should return the roman numerical value.  In the case that a non roman numeral string is passed in the function should return 'null'
+
+-- Examples --
+// ("XVI")   --> 16;
+// ("XIV")   --> 14;
+// ("XAV")   --> 'null';
+
+---------------------------------------------------------------------------- */
+// Provided Helper
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+// End of helpers
