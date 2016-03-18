cbfe2993569ef981ceee6fcd344fa8efc949a784
Christopher Decker <christopher.w.decker@gmail.com>
2016-03-18 16:28:10 -0700

    [new] add a new problem for the day BubbleSort

---
+++ BubbleSort.js
@@ -0,0 +1,12 @@
+/* ---------------------------------------------------------------------------
+-- Bubble Sort --
+One of the most basic forms of sorting through an array, where you compaire the current value with the next value and rearrange them into the proper order.
+
+The purpose it write your own implemntation that uses these same principles.  The function itself will take in an array list and should return a sorted list.
+
+-- Examples --
+
+bubbleSort([6,2,4,7,457,4,3]);    --> [2,3,4,4,6,7,457]
+bubbleSort([1,-1,6,3]);           --> [-1,1,3,6]
+
+--------------------------------------------------------------------------- */
