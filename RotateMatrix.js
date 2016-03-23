f8b66f69a3e91011ebbbdc5c349db2b09d1c8f90
Christopher Decker <christopher.w.decker@gmail.com>
2016-03-22 17:48:51 -0700

    [new] add a new problem for the day Rotate Matrix

---
+++ RotateMatrix.js
@@ -0,0 +1,14 @@
+/* ------------------------------------------------------------------------------------------------------------------
+-- Rotate Matrix --
+The function will be passed in a matrix array, and you need to return a matrix rotated 90 degrees. The arrays should be able to hold any type of value
+
+-- Example --
+rotateMatrix([                          [
+  [1,2,3],          -- should return-->   [7,4,1]
+  [4,5,6],                                [8,5,2]
+  [7,8,9]                                 [9,6,3]
+]);                                     ]
+
+rotateMatrix [ [1,2], [3,4] ];         --> [ [3,1], [4,2] ]
+
+------------------------------------------------------------------------------------------------------------------ */
